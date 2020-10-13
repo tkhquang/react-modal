d9c47088362328dc6365209fc7edbb6a4b06deea
Quang Trinh <khacquang.trinh@gmail.com>
2020-10-13 07:15:46 +0700

    Updates

---
+++ precache-manifest.0f715e48c95dd1761add201d552441c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf260e985889f07b98de224eb7df459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c15f976be73db3658dd",
+    "url": "/static/css/main.51743701.chunk.css"
+  },
+  {
+    "revision": "b1529ba66eba246cea7f",
+    "url": "/static/js/2.01de4894.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.01de4894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c15f976be73db3658dd",
+    "url": "/static/js/main.eaff6c22.chunk.js"
+  },
+  {
+    "revision": "caadec48116029e9fe7c",
+    "url": "/static/js/runtime-main.5de9298a.js"
+  }
+]);
